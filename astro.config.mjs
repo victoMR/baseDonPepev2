88711dcb5811e70793d7048d8f088af402aa30f6
victoMR <ratonrangelmejia+2@gmail.com>
2023-06-27 15:45:19 -0600

    commit general

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
